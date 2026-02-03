9747bf93a008e225d4ae20b6aa045afd2f3042d0
enswapwar <115549512+enswapwar@users.noreply.github.com>
2026-02-03 14:37:59 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
 app.use(express.json());
-app.use("/public", express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/input", (req, res) => {
   console.log("受信:", req.body);
   res.sendStatus(200);
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
